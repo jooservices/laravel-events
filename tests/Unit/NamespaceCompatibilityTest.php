f13d74250bfbbef1bc68a87e1f183a6062b89f73
Viet Vu <jooservices@gmail.com>
2026-07-26 12:18:16 -0600

    fix(namespace): drop legacy alias compatibility tests

--- tests/Unit/NamespaceCompatibilityTest.php
+++ tests/Unit/NamespaceCompatibilityTest.php
@@ -4,42 +4,33 @@ declare(strict_types=1);
 
 namespace JOOservices\LaravelEvents\Tests\Unit;
 
+use JOOservices\LaravelEvents\EventLog\Concerns\DefaultsToUpdatedAction;
+use JOOservices\LaravelEvents\EventLog\Contracts\LoggableModelInterface;
 use JOOservices\LaravelEvents\EventService;
+use JOOservices\LaravelEvents\EventSourcing\Concerns\HasEventSourcingDefaults;
+use JOOservices\LaravelEvents\EventSourcing\Contracts\EventSourcingInterface;
 use JOOservices\LaravelEvents\Tests\TestCase;
 use ReflectionClass;
 
 class NamespaceCompatibilityTest extends TestCase
 {
-    private const LEGACY_NAMESPACE = 'JOOservices\\LaravelEvents\\';
-
-    private const LEGACY_EVENT_SOURCING_INTERFACE = self::LEGACY_NAMESPACE
-        .'EventSourcing\\Contracts\\EventSourcingInterface';
-
-    private const LEGACY_LOGGABLE_MODEL_INTERFACE = self::LEGACY_NAMESPACE
-        .'EventLog\\Contracts\\LoggableModelInterface';
-
-    private const LEGACY_EVENT_SOURCING_DEFAULTS = self::LEGACY_NAMESPACE
-        .'EventSourcing\\Concerns\\HasEventSourcingDefaults';
-
-    private const LEGACY_UPDATED_ACTION_DEFAULTS = self::LEGACY_NAMESPACE
-        .'EventLog\\Concerns\\DefaultsToUpdatedAction';
-
-    public function test_legacy_class_namespace_resolves_to_canonical_class(): void
+    public function test_canonical_class_namespace_resolves(): void
     {
-        $legacyClass = 'JOOservices\\LaravelEvents\\EventService';
+        $class = EventService::class;
 
-        $this->assertTrue(class_exists($legacyClass));
+        $this->assertTrue(class_exists($class));
         $this->assertSame(
             EventService::class,
-            (new ReflectionClass($legacyClass))->getName()
+            (new ReflectionClass($class))->getName()
         );
+        $this->assertStringStartsWith('JOOservices\\LaravelEvents\\', $class);
     }
 
-    public function test_legacy_interface_and_trait_namespaces_are_available(): void
+    public function test_canonical_interface_and_trait_namespaces_are_available(): void
     {
-        $this->assertTrue(interface_exists(self::LEGACY_EVENT_SOURCING_INTERFACE));
-        $this->assertTrue(interface_exists(self::LEGACY_LOGGABLE_MODEL_INTERFACE));
-        $this->assertTrue(trait_exists(self::LEGACY_EVENT_SOURCING_DEFAULTS));
-        $this->assertTrue(trait_exists(self::LEGACY_UPDATED_ACTION_DEFAULTS));
+        $this->assertTrue(interface_exists(EventSourcingInterface::class));
+        $this->assertTrue(interface_exists(LoggableModelInterface::class));
+        $this->assertTrue(trait_exists(HasEventSourcingDefaults::class));
+        $this->assertTrue(trait_exists(DefaultsToUpdatedAction::class));
     }
 }
